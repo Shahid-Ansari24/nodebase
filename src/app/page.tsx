301edbbe34f1c36b7e47ff39968f9fd4af647b68
Shahid <shahidansari2452@gmail.com>
2026-05-04 22:50:50 +0530

    feat: added trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
-"use client"
-
-import { Button } from "@/components/ui/button";
-import { useEffect } from "react";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { Client } from "./client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 
 const Page = () => {
-  useEffect (() => {
-    console.log("Page component mounted");
-  }, []);
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
 
   return (    
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      {/* <h1>Welcome to the Home Page</h1>
-      <p>This is the main landing page of the application.</p> */}
-      <Button>Click me</Button>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Client />
+        </Suspense>
+      </HydrationBoundary>
     </div>
   )
+  
 }
 
 export default Page
