cc3d29e0e2fdbd46aeac5a37ebfef8620f4a307e
Shahid <shahidansari2452@gmail.com>
2026-05-19 00:36:44 +0530

    feat(auth): add social login options with GitHub and Google icons
    - Added GitHub and Google SVG logos to the project.
    - Updated LoginForm and RegisterForm components to include buttons for social login with GitHub and Google.
    - Created AuthLayout component to wrap authentication pages with a consistent layout.
    - Refactored layout.tsx to use AuthLayout for better structure.
    - Simplified page components for login and signup by removing unnecessary div wrappers.
    - Updated global styles for improved design consistency.

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,13 @@
 import { LoginForm } from "@/features/auth/components/login-form";
 import { requireUnauth } from "@/lib/auth-utils";
+import Link from "next/link";
+import Image from "next/image";
+
 
 const Page = async () => {
     await requireUnauth();
     return (
-        <div>
-            <LoginForm />
-        </div>
+        <LoginForm />
     )
 };
 
--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -81,6 +81,7 @@ export function LoginForm() {
                                         type="button"
                                         disabled={isPending}
                                     >
+                                        <Image alt="Github" src="/logos/github.svg" width={20} height={20} />
                                         Continue with Github
                                     </Button>
                                     <Button
@@ -89,6 +90,7 @@ export function LoginForm() {
                                         type="button"
                                         disabled={isPending}
                                     >
+                                        <Image alt="Google" src="/logos/google.svg" width={20} height={20} />
                                         Continue with Google
                                     </Button>
                                 </div>
